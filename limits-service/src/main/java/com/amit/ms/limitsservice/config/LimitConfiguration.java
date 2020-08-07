19a07fd0758eeb547ac8a16a4b415288a646374c
Amit Singh <amit8829.jnnce@gmail.com>
2020-08-07 07:30:59 +0530

    fault tolerant update
    fault tolerant update

--- limits-service/src/main/java/com/amit/ms/limitsservice/config/LimitConfiguration.java
+++ limits-service/src/main/java/com/amit/ms/limitsservice/config/LimitConfiguration.java
@@ -1,5 +1,6 @@
 package com.amit.ms.limitsservice.config;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,16 @@ public class LimitConfiguration {
     @GetMapping(path = "/limits")
     public Configuration getConfiguration() {
         return new Configuration(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+    //Demo for Hystrix fault tolerant
+    @GetMapping(path = "/fault-tolerant-limits")
+    @HystrixCommand(fallbackMethod = "fallbackRateLimit")
+    public Configuration getConfigurationFaultTolerant() {
+        throw new RuntimeException("Not found");
+    }
+
+    public Configuration fallbackRateLimit() {
+        return new Configuration(configuration.getMinimum(),configuration.getMaximum());
     }
 }
